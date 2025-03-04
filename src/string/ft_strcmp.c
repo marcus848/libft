93aab4022788ad5147f178dbb65e7a9d49922893
Marcus Gomes <marcudos42@mydeb.roteador.multilaser.com>
2025-03-03 21:48:34 -0300

    add ft_strcmp.c

---
+++ src/string/ft_strcmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcudos <marcudos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 17:31:11 by marcudos          #+#    #+#             */
+/*   Updated: 2024/12/17 20:36:24 by marcudos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while ((*s1 || *s2))
+	{
+		if ((unsigned char)*s1 != (unsigned char)*s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
